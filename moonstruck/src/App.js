31e45d54d1a4fce6fac58e98eb7fa5d55be57497
Gimhanihangawaththa <gimhani0327@gmail.com>
2025-01-24 02:40:46 +0530

    change-nav

--- moonstruck/src/App.js
+++ moonstruck/src/App.js
@@ -1,13 +1,31 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
-
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/SHOP-BY-BRANDS' element={<ShopCategory category='SHOP-BY-BRANDS'/>}/>
+        <Route path='/NEW-ARRIVALS' element={<ShopCategory category='SHOP-BY-BRANDS'/>}/>
+        <Route path='/COLLECTIONS' element={<ShopCategory category='COLLECTIONS'/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- moonstruck/src/Components/Navbar/Navbar.jsx
+++ moonstruck/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import cart_icon from'../Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
 
 
 
@@ -12,8 +13,8 @@ const Navbar = () => {
       <div className='up'>
         <div className="nav-logo">
         <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt='' />
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/Cart'><img src={cart_icon} alt='' /></Link>
         <div className="nav-cart-count">0</div>
       </div>
       </div>
@@ -21,10 +22,10 @@ const Navbar = () => {
       
       <div className="nav-minibar">
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("HOME")}}>HOME{menu==="HOME"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("SHOP BY BRANDS")}}>SHOP BY BRANDS{menu==="SHOP BY BRANDS"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("NEW ARRIVALS")}}>NEW ARRIVALS{menu==="NEW ARRIVALS"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("COLLECTIONS")}}>COLLECTIONS{menu==="COLLECTIONS"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("HOME")}}><Link to='/'>HOME</Link>{menu==="HOME"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("SHOP-BY-BRANDS")}}><Link to='/SHOP-BY-BRANDS'>SHOP BY BRANDS</Link>{menu==="SHOP-BY-BRANDS"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("NEW-ARRIVALS")}}><Link to='/NEW ARRIVALS'>NEW ARRIVALS</Link>{menu==="NEW-ARRIVALS"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("COLLECTIONS")}}><Link to='/COLLECTIONS'>COLLECTIONS</Link>{menu==="COLLECTIONS"?<hr/>:<></>}</li>
       </ul>
       </div>
       
