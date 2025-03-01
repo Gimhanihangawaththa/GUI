62b013342a91298484d5026258ee03589c2838b7
Gimhanihangawaththa <gimhani0327@gmail.com>
2025-03-01 19:59:21 +0530

    create popular

---
+++ moonstruck/src/Components/Assets/data.js
@@ -0,0 +1,49 @@
+
+import p1_img from './p1_img.jpg'
+import p2_img from './p2_img.jpg'
+import p3_img from './p3_img.jpg'
+import p4_img from './p4_img.jpg'
+import p5_img from './p5_img.jpg'
+
+let data_product = [
+    {
+        id : 1,
+        name:"Wrap Skirt",
+        image:p1_img,
+        new_price:'5,550',
+        old_price:'6,500',
+
+
+    },
+    {
+        id :2,
+        name:"Long Sleeve Smocked Top",
+        image:p2_img,
+        new_price:'5,920',
+        old_price:'6,570',
+
+    },
+    {
+        id:3,
+        name:"Wide Leg Pants",
+        image:p3_img,
+        new_price:'7,150',
+        old_price:'6,595',
+
+    },
+    {
+        id:4,
+        name:"Embroidery Maxi Dress",
+        imge:p4_img,
+        new_price:'12,700',
+        old_price:'13,900',
+    },
+    {
+        id:5,
+        name:"Embroidery Maxi Dress",
+        imge:p5_img,
+        new_price:'12,700',
+        old_price:'13,900',
+    }
+]
+export default data_product;
